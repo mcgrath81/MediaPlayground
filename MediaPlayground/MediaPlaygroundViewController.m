dceaf147de57cc79cab0cccb176687349f5f5c32
admin <admin@admins-MacBook-4.local>
2013-02-14 14:25:49 +0000

    Initial Commit

---
+++ MediaPlayground/MediaPlaygroundViewController.m
@@ -0,0 +1,29 @@
+//
+//  MediaPlaygroundViewController.m
+//  MediaPlayground
+//
+//  Created by admin on 14/02/2013.
+//  Copyright (c) 2013 emuse. All rights reserved.
+//
+
+#import "MediaPlaygroundViewController.h"
+
+@interface MediaPlaygroundViewController ()
+
+@end
+
+@implementation MediaPlaygroundViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
